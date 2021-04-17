274dc6b3dd0ef757e3b5237a5ee8e6217d03cd3f
Matheus Madeira <matheus.madeira@townsq.com.br>
2021-04-17 20:19:55 -0300

    Ploting GrLivArea by ids

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Trabalho 01 - Parte 3
 Nomes: Matheus Severo Madeira (00275621), xxxx xxx (00xxxxx)
 ## Dependências
+- matplotlib
+  - Script de instalacao: python3 -m pip install matplotlib
 - Numpy
   - Script de instalação: python3 -m pip install numpy
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello world')
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('house_prices_train.csv', delimiter=',')
+
+#Extrair colunas para análise
+ids = np.array(data[:, 0])
+living_area = np.array(data[:, 46])
+
+#Gráfico dos dados
+plt.figure(figsize=(20, 12))
+plt.scatter(ids, living_area)
+plt.xlabel('Ids')
+plt.ylabel('Área da Sala de Estar')
+plt.title('Dados')
+plt.show()
